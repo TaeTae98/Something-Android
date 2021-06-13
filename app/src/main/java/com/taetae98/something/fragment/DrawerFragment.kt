28b04de0a81a812f6756f41ea5014a4018b5a3af
KangTaeJong98 <rkdxowhd98@naver.com>
2021-06-13 17:49:24 +0900

    ST-2 MainActivity BottomNavigationView 연동

---
+++ app/src/main/java/com/taetae98/something/fragment/DrawerFragment.kt
@@ -0,0 +1,7 @@
+package com.taetae98.something.fragment
+
+import com.taetae98.something.R
+import com.taetae98.something.base.BindingFragment
+import com.taetae98.something.databinding.FragmentDrawerBinding
+
+class DrawerFragment : BindingFragment<FragmentDrawerBinding>(R.layout.fragment_drawer)
---
+++ app/src/main/java/com/taetae98/something/fragment/ToDoFragment.kt
@@ -0,0 +1,7 @@
+package com.taetae98.something.fragment
+
+import com.taetae98.something.R
+import com.taetae98.something.base.BindingFragment
+import com.taetae98.something.databinding.FragmentTodoBinding
+
+class ToDoFragment : BindingFragment<FragmentTodoBinding>(R.layout.fragment_todo)
